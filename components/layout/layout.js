57e2851e13268825024bd983faa8c9344eac0e93
Richard Lovell <richjavalabs@gmail.com>
2022-06-07 22:38:35 +1200

    Updated with layout flag

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,14 +2,14 @@ const Layout = (props) => {
   const { children, page } = props;
   return (
     <>
-      {page.layout && (
+      {page.layout && page.layout._0 && (
         <page.layout._0.component
           content={page.layout._0.content}
           app={page.app}
         />
       )}
       <main>{children}</main>
-      {page.layout && (
+      {page.layout && page.layout._1 && (
         <page.layout._1.component
           content={page.layout._1.content}
           app={page.app}
